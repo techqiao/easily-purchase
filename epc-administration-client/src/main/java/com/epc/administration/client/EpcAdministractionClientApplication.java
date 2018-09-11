2fdfaebb3b73a823c45cc032926cbfcac8b081d2
lijun <lijun@difengshanguo.com>
2018-09-11 22:25:08 +0800

    swagger

--- epc-administration-client/src/main/java/com/epc/administration/client/EpcAdministractionClientApplication.java
+++ epc-administration-client/src/main/java/com/epc/administration/client/EpcAdministractionClientApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
--- epc-administration-client/src/main/java/com/epc/administration/client/controller/operator/OperatorController.java
+++ epc-administration-client/src/main/java/com/epc/administration/client/controller/operator/OperatorController.java
@@ -3,6 +3,9 @@ package com.epc.administration.client.controller.operator;
 import com.epc.administration.client.remoteapi.operator.OperatorClient;
 import com.epc.administration.facade.operator.handle.HandleOperatorBasicInfo;
 import com.epc.common.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +18,15 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping(value = "/operator", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Api(value = "运营商服务")
 public class OperatorController {
     @Autowired
     private OperatorClient operatorClient;
 
+
+    @ApiOperation(value = "添加运营商")
     @PostMapping(value = "insertOperatorBasicInfo")
-    public Result<Boolean> insertOperatorBasicInfo(HandleOperatorBasicInfo handleOperatorBasicInfo) {
+    public Result<Boolean> insertOperatorBasicInfo(@ApiParam HandleOperatorBasicInfo handleOperatorBasicInfo) {
         return operatorClient.insertOperatorBasicInfo(handleOperatorBasicInfo);
     }
 

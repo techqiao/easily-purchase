23e21423a76929aca0e65ceaed701b9e37a49ff9
linzhixiang <615984601@qq.com>
2018-09-15 10:59:05 +0800

    up

---
+++ epc-web-client/src/main/java/com/epc/web/client/swagger/Swagger2.java
@@ -0,0 +1,28 @@
+package com.epc.web.client.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(
+                RequestHandlerSelectors.basePackage("com.epc.web.client")).paths(
+                PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("admin web center").description("后台 openApi").termsOfServiceUrl(
+                "http://127.0.0.1:4101/swagger-ui.html").version("1.0").build();
+    }
+}

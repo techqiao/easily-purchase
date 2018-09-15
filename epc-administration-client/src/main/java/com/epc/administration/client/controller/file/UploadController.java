c92d16dafd9f0ed8672e35a4ff6e8f2275f1a36f
wujiangqiao <247021523@qq.com>
2018-09-15 10:29:07 +0800

    swagger 乱码

--- epc-administration-client/src/main/java/com/epc/administration/client/controller/file/UploadController.java
+++ epc-administration-client/src/main/java/com/epc/administration/client/controller/file/UploadController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,6 +25,7 @@ import java.util.Date;
  * <p>Date : 2018-09-11  22:50
  * <p>@author : wjq
  */
+@ApiIgnore
 @RestController
 @RequestMapping("/upload")
 public class UploadController {

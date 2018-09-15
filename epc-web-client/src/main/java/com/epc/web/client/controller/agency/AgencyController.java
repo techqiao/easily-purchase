a54eb108153c4dcdd0cfc8c505bba6669b1b2a2f
winlin <winlin_chan@126.com>
2018-09-15 17:07:37 +0800

    信息更新

--- epc-web-client/src/main/java/com/epc/web/client/controller/agency/AgencyController.java
+++ epc-web-client/src/main/java/com/epc/web/client/controller/agency/AgencyController.java
@@ -6,7 +6,10 @@ import com.epc.web.facade.agency.FacadeAgencyService;
 import com.epc.web.facade.agency.handle.HandleEmployee;
 import com.epc.web.facade.agency.handle.HandleExpert;
 import com.epc.web.facade.agency.handle.HandleSupplier;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,24 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description :
  * @Date:2018/9/15
  */
-@RequestMapping(value = "/agency", method = RequestMethod.POST)
+@Api(value = "代理机构")
+@RequestMapping(value = "/agency", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @RestController
 public class AgencyController  {
 
     @Autowired
     AgencyClient agencyClient;
 
-    @PostMapping(value = "/insertEmployeee", consumes = "application/json;charset=utf-8", produces = "application/json;charset=utf-8")
+    @ApiOperation(value = "代理机构新增员工" , tags = "新增代理机构员工")
+    @PostMapping(value = "/insertEmployeee")
     public Result insertEmployee(HandleEmployee employee) {
         return agencyClient.insertEmployee(employee);
     }
 
-    @PostMapping(value = "/insertExpert", consumes = "application/json;charset=utf-8", produces = "application/json;charset=utf-8")
+    @ApiOperation(value = "代理机构新增专家" , tags = "新增代理机构专家")
+    @PostMapping(value = "/insertExpert")
     public Result insertExpert(HandleExpert handleExpert) {
         return agencyClient.insertExpert(handleExpert);
     }
 
-    @PostMapping(value = "/insertSupplier", consumes = "application/json;charset=utf-8", produces = "application/json;charset=utf-8")
+    @ApiOperation(value = "代理机构新增供货商" , tags = "新增代理机构供货商")
+    @PostMapping(value = "/insertSupplier")
     public Result insertSupplier(HandleSupplier handleSupplier) {
         return agencyClient.insertSupplier(handleSupplier);
     }

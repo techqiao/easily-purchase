36e1210076a8e941dc9bb992c2e91d4dc40af04b
wujiangqiao <247021523@qq.com>
2018-09-17 21:51:06 +0800

    Result

--- epc-common-util/src/main/java/com/epc/common/constants/ErrorMessagesEnum.java
+++ epc-common-util/src/main/java/com/epc/common/constants/ErrorMessagesEnum.java
@@ -8,45 +8,45 @@ package com.epc.common.constants;
  */
 public enum ErrorMessagesEnum {
     
-    HTTPCLIENT_ERROR("HTTPCLIENT_ERROR","http链接失败"),
-    NOT_AUTHORIZED("401","身份验证失败"),
-    LOGIN_USER_LOGOUT_ERROR("LOGIN_USER_LOGOUT_ERROR","登出失败"),
-	LOGIN_NAME_OR_PSW_MISTAKE("LoginName or psw mistake","登录名或密码错误"),
-	LOGINNAME_DOES_NOT_EXIST("LoginName does not exist","登录账号不存在"),
-	LOGINNAME_EXIST("LoginName  exist","账号已存在"),
-	LOGINNAME_DISABLED("Has been disabled, please contact the administrator","已禁用，请联系管理员"),
-	PASSWORD_ERROR("Password error","密码错误"),
-	LOGIN_USER_LOGIN_ERROR("Login user login error","登陆异常"),
-	ORIGINAL_PASSWORD_INPUT_ERROR("Original password input error","原始密码输入错误"),
-    FAILURE("Operation failed","操作失败"),
-    INSERT_FAILURE("Increase failed","增加失败"),
-    UPDATE_FAILURE("Update failed","更改失败"),
-    SELECT_FAILURE("Search failed","查询失败"),
-    DELETE_FAILURE("Delete failed","删除失败"),
-    CHANGE_FAILURE("Switch views","转换失败"),
-    COOKIE_SEARCH_FAILUE("Cookie search failed","cookie查询失败"),
-    INSTANCE_NOT_EXIST("Instance not exist","对象为空"),
-    CATCODE_IS_EXIST("CATCODE_IS_EXIST","编码已存在"),
-    ID_ILLEAGAL("ID illeagal","ID非法"),
-    PARAMETER_IS_NULL("Parameter list is empty","参数列表为空"),
-    INCOMPLETE_PARAMETER("Incomplete parameter","参数不全"),
-    RECORD_IS_NULL("Record is null","数据不存在"),
-    LOGINNAME_NUMBER_EXIST("LoginName number exist","账号已存在"),
-    REOCRD_IS_DELETE("REOCRD_IS_DELETE","数据已删除"),
-    FILE_UPLOAD_ERROR("FILE_UPLOAD_ERROR","文件上传失败"),
-    IDENTIFYING_CODE_SEND_ERROR("IDENTIFYING_CODE_SEND_ERROR","验证码发送失败"),
-    FILE_EMPTY("FILE_EMPTY","无文件信息"),
-    BANKCARD_NO_IS_EXIST("BANKCARD_NO_IS_EXIST","银行卡号已添加"),
+    HTTPCLIENT_ERROR(10001,"http链接失败"),
+    NOT_AUTHORIZED(1002,"身份验证失败"),
+    LOGIN_USER_LOGOUT_ERROR(1003,"登出失败"),
+	LOGIN_NAME_OR_PSW_MISTAKE(1004,"登录名或密码错误"),
+	LOGINNAME_DOES_NOT_EXIST(10005,"登录账号不存在"),
+	LOGINNAME_EXIST(1006,"账号已存在"),
+	LOGINNAME_DISABLED(1007,"已禁用，请联系管理员"),
+	PASSWORD_ERROR(1008,"密码错误"),
+	LOGIN_USER_LOGIN_ERROR(1009,"登陆异常"),
+	ORIGINAL_PASSWORD_INPUT_ERROR(1010,"原始密码输入错误"),
+    FAILURE(1011,"操作失败"),
+    INSERT_FAILURE(1012,"增加失败"),
+    UPDATE_FAILURE(1013,"更改失败"),
+    SELECT_FAILURE(1014,"查询失败"),
+    DELETE_FAILURE(1015,"删除失败"),
+    CHANGE_FAILURE(1016,"转换失败"),
+    COOKIE_SEARCH_FAILUE(1017,"cookie查询失败"),
+    INSTANCE_NOT_EXIST(1018,"对象为空"),
+    CATCODE_IS_EXIST(1019,"编码已存在"),
+    ID_ILLEAGAL(1020,"ID非法"),
+    PARAMETER_IS_NULL(1021,"参数列表为空"),
+    INCOMPLETE_PARAMETER(1022,"参数不全"),
+    RECORD_IS_NULL(1023,"数据不存在"),
+    LOGINNAME_NUMBER_EXIST(1024,"账号已存在"),
+    REOCRD_IS_DELETE(1025,"数据已删除"),
+    FILE_UPLOAD_ERROR(1026,"文件上传失败"),
+    IDENTIFYING_CODE_SEND_ERROR(1027,"验证码发送失败"),
+    FILE_EMPTY(1028,"无文件信息"),
+    BANKCARD_NO_IS_EXIST(1029,"银行卡号已添加"),
     ;
-    private final String errCode;
+    private final Integer errCode;
     private final String errInfo;
     
-    ErrorMessagesEnum(String errCode, String errInfo){
+    ErrorMessagesEnum(Integer errCode, String errInfo){
         this.errInfo = errInfo;
         this.errCode = errCode;
     }
 
-	public String getErrCode() {
+	public Integer getErrCode() {
 		return errCode;
 	}
 

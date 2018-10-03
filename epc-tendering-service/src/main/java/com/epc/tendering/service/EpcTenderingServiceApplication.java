70fbd1edd5d412a3e07b56e977810e7bafaea834
lijun <lijun@difengshanguo.com>
2018-10-03 13:45:08 +0800

    cimmit

--- epc-tendering-service/src/main/java/com/epc/tendering/service/EpcTenderingServiceApplication.java
+++ epc-tendering-service/src/main/java/com/epc/tendering/service/EpcTenderingServiceApplication.java
@@ -1,7 +1,9 @@
 package com.epc.tendering.service;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.epc.tendering.service.mapper")
 @EnableScheduling
 public class EpcTenderingServiceApplication {

0107fd0cd2c5e5450c8ab0844056d25161a29305
donghuan <d_donghuan@126.com>
2018-10-09 09:34:51 +0800

    operator up

--- epc-web-client/src/main/java/com/epc/web/client/controller/terdering/project/handle/ClientHandleCreateProjectByAdmin.java
+++ epc-web-client/src/main/java/com/epc/web/client/controller/terdering/project/handle/ClientHandleCreateProjectByAdmin.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-@ApiModel(value = "ClientHandleCreateProjectByAdmin",description = "指派项目负责人(在项目人员指派关系表 t_project_employee_relation 插入一条数据)")
+@ApiModel(value = "ClientHandleCreateProjectByAdmin",description = "指派项目经理(在项目人员指派关系表 t_project_employee_relation 插入一条数据)")
 @Data
 public class ClientHandleCreateProjectByAdmin {
 
@@ -12,17 +12,15 @@ public class ClientHandleCreateProjectByAdmin {
     private String projectName;
 
     @ApiModelProperty(value = "当前指派人id(管理员id或是法人id)")
-    private Long createrId;
+    private Long executiveId;
 
-    @ApiModelProperty(value = "当前的角色 role(是法人还是管理员)")
-    private Integer role;
 
     @ApiModelProperty(value = "创建项目人 姓名")
-    private String createrName;
+    private String executiveName;
 
+    @ApiModelProperty(value = "备注")
+    private String notes;
 
-    @ApiModelProperty(value = "指派人的id(指派某个员工)")
-    private Long executiveId;
 
 
 }

27cba6a493c9f4ebfa55c0475012cf4334eab04f
donghuan <d_donghuan@126.com>
2018-10-09 09:34:58 +0800

    operator up

---
+++ epc-web-client/src/main/java/com/epc/web/client/controller/terdering/project/handle/ClientHandleUpdateProjectAdmin.java
@@ -0,0 +1,41 @@
+package com.epc.web.client.controller.terdering.project.handle;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(value = "ClientHandleUpdateProjectAdmin",description = "修改项目")
+public class ClientHandleUpdateProjectAdmin{
+
+
+
+    /**
+     * 项目id
+     */
+    @ApiModelProperty(value = "项目id")
+    private Long projectId;
+
+    /**
+     * 项目名称
+     */
+    @ApiModelProperty(value = "项目名称")
+    private String projectName;
+
+    /**
+     * 项目经理
+     */
+    @ApiModelProperty(value = "项目经理")
+    private String executiveName;
+
+    @ApiModelProperty(value = "项目经理id")
+    private Long executiveId;
+
+    @ApiModelProperty(value = "备注")
+    private String notes;
+
+
+
+}

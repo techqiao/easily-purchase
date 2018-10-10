eaec5b8f248efe0c1218cb5d127422006409480e
winlin <winlin_chan@126.com>
2018-10-10 12:19:10 +0800

    up

---
+++ epc-web-user-service/src/main/java/com/epc/web/service/mapper/expert/BAssessmentCommitteeExpertMapper.java
@@ -0,0 +1,38 @@
+package com.epc.web.service.mapper.expert;
+
+import com.epc.web.service.domain.expert.BAssessmentCommitteeExpert;
+import com.epc.web.service.domain.expert.BAssessmentCommitteeExpertCriteria;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface BAssessmentCommitteeExpertMapper {
+    int countByExample(BAssessmentCommitteeExpertCriteria example);
+
+    int deleteByExample(BAssessmentCommitteeExpertCriteria example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(BAssessmentCommitteeExpert record);
+
+    int insertSelective(BAssessmentCommitteeExpert record);
+
+    List<BAssessmentCommitteeExpert> selectByExampleWithRowbounds(BAssessmentCommitteeExpertCriteria example, RowBounds rowBounds);
+
+    List<BAssessmentCommitteeExpert> selectByExample(BAssessmentCommitteeExpertCriteria example);
+
+    BAssessmentCommitteeExpert selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") BAssessmentCommitteeExpert record, @Param("example") BAssessmentCommitteeExpertCriteria example);
+
+    int updateByExample(@Param("record") BAssessmentCommitteeExpert record, @Param("example") BAssessmentCommitteeExpertCriteria example);
+
+    int updateByPrimaryKeySelective(BAssessmentCommitteeExpert record);
+
+    int updateByPrimaryKey(BAssessmentCommitteeExpert record);
+
+    List<BAssessmentCommitteeExpert> selectByExpertId(Long expertId);
+
+//    List<Integer> selectBidList(Long expertId);
+
+}

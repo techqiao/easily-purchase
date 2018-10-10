a2b6650783dd1c03534410602f660858b06b3b27
donghuan <d_donghuan@126.com>
2018-10-10 12:11:32 +0800

    新增 搜索operator or supplier接口

--- epc-web-user-service/src/main/java/com/epc/web/service/service/operator/OperatorService.java
+++ epc-web-user-service/src/main/java/com/epc/web/service/service/operator/OperatorService.java
@@ -114,9 +114,15 @@ public interface OperatorService {
      */
     Result<List<TPurchaserBasicInfoVO>> lookPurchaserList(HandleOperatorLoginInfo handleOperatorLoginInfo);
 
-    /**16
-     *  运营商新增采购人（不包括完善信息，只填写姓名，电话）
+    /**15.6
+     *通过手机号或者姓名来搜索自己拉的采购人
      */
+    Result<List<TPurchaserBasicInfoVO>> searchPurchaserSingle(HandleOperatorCreateSupplier handleOperatorCreateSupplier);
+
+
+        /**16
+         *  运营商新增采购人（不包括完善信息，只填写姓名，电话）
+         */
     Result<Boolean> createPurchaseByOperatorSimple(HandleOperatorCreateSupplier handleOperatorCreateSupplier);
 
 
@@ -137,5 +143,13 @@ public interface OperatorService {
      */
     Result<List<TSupplierBasicInfoVO>> lookSupplierList(HandleOperatorLoginInfo handleOperatorLoginInfo);
 
+    /**20
+     *通过手机号或者姓名来搜索自己拉的供应商
+     */
+    Result<List<TSupplierBasicInfoVO>> searchSupplierSingle(HandleOperatorCreateSupplier handleOperatorCreateSupplier);
 
-}
+
+
+
+
+    }

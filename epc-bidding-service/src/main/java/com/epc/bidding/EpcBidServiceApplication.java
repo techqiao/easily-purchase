fc69daf6c32146d9451f6f5f81f77740566f1d75
wujiangqiao <247021523@qq.com>
2018-10-12 18:25:54 +0800

    Update EpcBidServiceApplication.java

--- epc-bidding-service/src/main/java/com/epc/bidding/EpcBidServiceApplication.java
+++ epc-bidding-service/src/main/java/com/epc/bidding/EpcBidServiceApplication.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.epc.bidding")
 @MapperScan(value = "com.epc.bidding.mapper")
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class EpcBidServiceApplication {
 
 	public static void main(String[] args) {

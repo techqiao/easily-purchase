9b584515290f7b9d5269025a0c4a0e48c4417f5c
linzhixiang <6159846501@qq.com>
2018-10-13 09:59:08 +0800

    update
    Signed-off-by: linzhixiang <6159846501@qq.com>

--- epc-web-facade/src/main/java/com/epc/web/facade/bidding/dto/IsPayDTO.java
+++ epc-web-facade/src/main/java/com/epc/web/facade/bidding/dto/IsPayDTO.java
@@ -7,10 +7,15 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-
+/**
+ * @Description: 支付情况
+ * @Author: linzhixiang
+ * @Date: 2018/10/13
+ */ 
 @Data
 @ApiModel("支付情况")
 public class IsPayDTO implements Serializable {
+    private static final long serialVersionUID = 6965372380199209329L;
     @ApiModelProperty("是否支付成功")
     private  Boolean isPay;
     @ApiModelProperty("需要支付金额")
